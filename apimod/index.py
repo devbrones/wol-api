f5f910a00ca554eaa595a6a4e19ec092145ecc0c
BuildTools <unconfigured@null.spigotmc.org>
2021-12-30 20:37:44 +0100

    fixed a problem, got two

--- apimod/index.py
+++ apimod/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import os.path
+import os
 
 app = Flask(__name__)
 
@@ -7,17 +7,18 @@ if os.path.isfile("../error-reports"):
     print("Log | N | file error-reports exists")
 else:
     print("Log | N | file error-reports does not exist, creating")
+    os.mknod('../error-reports')
 
 @app.route("/")
 def ret_ok():
     return "ok!200!\n<p>The API is active</p>"
 @app.route("/is-online")
 def isonline():
-    return "[{"online":"true"}]"
+    return """[{"online":"true"}]"""
 
 @app.route("/report-error", methods=['POST'])
 def error_report():
     with open("../error-reports", "a") as errep:
-        errep.write(request.get_json())
+        errep.write(str(request.get_json()))
         return '', 204
 
