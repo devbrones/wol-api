af170ae091f7a9577c36611707f3c0d6fa474963
BuildTools <unconfigured@null.spigotmc.org>
2021-12-30 23:48:32 +0100

    we are now at 1.0.something

--- apimod/index.py
+++ apimod/index.py
@@ -9,16 +9,33 @@ else:
     print("Log | N | file error-reports does not exist, creating")
     os.mknod('/home/rex/projects/wol-api/error-reports')
 
-@app.route("/")
+@app.route("/api/")
 def ret_ok():
-    return "ok!200!\n<p>The API is active</p>"
-@app.route("/is-online")
+    return "ok!200!\n<h1>The API is active</h1>\n<p>Watch-on-LBRY API Version 1.0.9 by Devbrones https://tibroness.org</p>"
+
+@app.route("/api/is-online")
 def isonline():
     return """[{"online":"true"}]"""
 
-@app.route("/report-error", methods=['POST'])
+@app.route("/api/report-error", methods=['POST'])
 def error_report():
+    # report an error by sending a POST request to http://madinator.com/api/
     with open("/home/rex/projects/wol-api/error-reports", "a") as errep:
         errep.write(str(request.get_json()))
         return '', 204
 
+@app.route("/api/get-lbry-url", methods=['GET'])
+def getlurl():
+    # database management goes here, it will read the db for (request.args.get('url'))
+    # and if it is found it returns url, otherwise it would be wise to query the LBRY
+    # API and get the new url and store to db
+    # db structure ex:
+    # [
+    #   {
+    #   'yt-url':'dQw4w9WgXcQ',
+    #   'yt-title':'Rick Astley - Never Gonna Give You Up (Official Music Video)',
+    #   'lbry-url':'example_url',
+    #   'lbry-title':'pickle rick funny '
+    #   }
+    # ]
+    return request.args.get('url')
