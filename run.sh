b20f1efe619ce2ed9afc9e89a44623d9021dfb8a
Devbrones <jonas.cronholm@protonmail.com>
2022-01-04 23:59:59 +0100

    more stuff, check diff :D pt 2

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 # make flask find api application
 export FLASK_APP=./apimod/index.py
+# export LOG_PATH=./log
 
 # run flask listening on all interfaces in pipenv
 pipenv run python -m flask run --debugger -h 0.0.0.0
